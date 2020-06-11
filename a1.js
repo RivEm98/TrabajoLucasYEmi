8624c20ad759e5d30240e82ec562cf7669027c50
Lucas Vinotti <hp-lucas@hotmail.com>
2020-06-11 12:29:58 -0300

    guardo mis datos en a1

--- a1.js
+++ a1.js
@@ -1,1 +1,9 @@
 console.log("Este es el archivo 1!");
+
+let persona = {
+    nombre: "Lucas",
+    apellido: "Vinotti",
+    edad: 23
+}
+
+console.log("Mi nombre es" + persona.nombre + " " + persona.apellido + " " + "y mi edad es" + persona.edad + " " + "años.")

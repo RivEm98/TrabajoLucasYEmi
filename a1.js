71f3b03c90875f4973323f46fb3750aa7cbb7a50
Emiliano Rivarola <emi_rivarola@hotmail.com>
2020-06-11 12:28:06 -0300

    guardo mis datos personales

--- a1.js
+++ a1.js
@@ -1,1 +1,6 @@
 console.log("Este es el archivo 1!");
+
+let nombre = "Emiliano";
+let apellido = "Rivarola";
+let edad = 22;
+console.log("Mi nombre es " + nombre + " " + apellido + " " + "y tengo " + edad + " años!");
